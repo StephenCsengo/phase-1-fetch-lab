4784550a4ba87735b7fa512bf9445176fa13317d
Stephen Csengo <stephencsengo@gmail.com>
2023-04-19 14:29:56 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(function (resp){
+    let data = resp.json();
+    return data;
+  })
+  .then(renderBooks);
 }
 
 function renderBooks(books) {
